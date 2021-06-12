fce7cdf49a0af3b978750a3fd5eea76ff590a111
ariefg <ariefgraifhan@gmail.com>
2021-06-12 22:55:08 +0700

    nambah fungsi show dan index

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -7,4 +7,19 @@ use App\Models\Product;
 
 class ItemController extends Controller
 {
+    public function __construct() {
+        $this->product = new Product();
+    }
+
+    public function index()
+    {
+        $data['products'] = $this->product->getAllData();
+        return view('index', $data); 
+    }
+
+    public function show($id)
+    {
+        $data['product'] = $this->product->getData($id);
+        return view('product.detail.'.$id, $data);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function getAllData()
+    {
+        DB::table('products')->get();
+    }
+
+    public function getData($id)
+    {
+        DB::table('products')->where('id', '=', $id)->get();
+    }
 }
